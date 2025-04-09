8caa0e19cf80a273b1bbddc66f13418c8dcc83cb
Olga <okatranis@gmail.com>
2025-04-09 17:38:25 +0300

    feat: create  throttledFromAmountChange,  throttledToAmountChange functions

--- vite-project/src/hooks/useCurrencyConverter.ts
+++ vite-project/src/hooks/useCurrencyConverter.ts
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { throttle } from 'lodash'; 
+
 import { fetchExchangeRate } from "../utils/fetchExchangeRate";
 import { currencyCodes } from "../data/currencyCodes";
 
@@ -23,16 +25,24 @@ const useCurrencyConverter = () => {
     getExchangeRate();
   }, [fromCurrency, toCurrency]);
 
-  const handleFromAmountChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const value: number = parseFloat(e.target.value);
+  const throttledFromAmountChange = throttle((value: number): void => {
     setFromAmount(value);
     setToAmount(parseFloat((value * conversionRate).toFixed(2)));
+  }, 300);
+
+  const throttledToAmountChange = throttle((value: number): void => {
+    setToAmount(value);
+    setFromAmount(parseFloat((value / conversionRate).toFixed(2)));
+  }, 300);
+
+  const handleFromAmountChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const value: number = parseFloat(e.target.value);
+    throttledFromAmountChange(value);
   };
 
   const handleToAmountChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const value: number = parseFloat(e.target.value);
-    setToAmount(value);
-    setFromAmount(parseFloat((value / conversionRate).toFixed(2)));
+    throttledToAmountChange(value);
   };
 
   const handleFromCurrencyChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
